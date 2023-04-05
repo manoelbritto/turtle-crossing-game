8008c9c5b2845ac31c9754c48297eabaf5306d47
Manoel Britto <manoelbritto@gmail.com>
2023-04-05 15:43:27 -0400

    initial code

--- player.py
+++ player.py
@@ -1,7 +1,23 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
+UP = 90
 
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.color("black")
+        self.game_start()
 
-class Player:
-    pass
+    def game_start(self):
+        self.settiltangle(90)
+        self.goto(STARTING_POSITION)
+
+    def up(self):
+        self.setheading(UP)
+        self.settiltangle(0)
+        self.forward(MOVE_DISTANCE)
